79dd7546fb135bbb2dcf8e2cfe6cf227f87ae08c
oussamahaouder <66519276+oussamahaouder@users.noreply.github.com>
2021-12-20 20:59:17 +0100

    comment

--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,7 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   if (req.body.password) {
     req.body.password = CryptoJS.AES.encrypt(
       req.body.password,
-      process.env.PASS_SEC
+      process.env.PASS_SEC //Process
     ).toString();
   }
 
